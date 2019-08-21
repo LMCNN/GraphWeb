057403172cc4aa1ae36a4934af23a58b0348fcd0
LMCNN <lmcn@vt.edu>
2019-08-21 14:39:01 +0800

    small changes

--- static/graph/graph.js
+++ static/graph/graph.js
@@ -10,22 +10,28 @@ $(document).ready(function() {
     var i,
         s,
         N = 100,
-        E = 500,
         g = {
             nodes: [],
             edges: []
-        };
+        },
+        colors = [
+            '#617db4',
+            '#668f3c',
+            '#c6583e',
+            '#b956af',
+            '#b9ae2f'
+        ];
 
     // Generate a random graph:
     g.nodes.push(
-        {id:'0', label:'Character', x:Math.random(), y:Math.random(), size:20, color:'#1e6617'},
-        {id:'1', label:'Character', x:Math.random(), y:Math.random(), size:20, color:'#1e6617'},
-        {id:'2', label:'User', x:Math.random(), y:Math.random(), size:20, color:'#332f66'},
-        {id:'3', label:'User', x:Math.random(), y:Math.random(), size:20, color:'#332f66'},
-        {id:'4', label:'Number', x:Math.random(), y:Math.random(), size:20, color:'#663a37'},
-        {id:'5', label:'Number', x:Math.random(), y:Math.random(), size:20, color:'#663a37'},
-        {id:'6', label:'APP', x:Math.random(), y:Math.random(), size:20, color:'#49662c'},
-        {id:'7', label:'Website', x:Math.random(), y:Math.random(), size:20, color:'#66355f'});
+        {id:'0', label:'Character', x:100 * Math.cos(2 * Math.PI / N), y:100 * Math.sin(2 * Math.PI / N), size:20, color:colors[0]},
+        {id:'1', label:'Character', x:100 * Math.cos(4 * Math.PI / N), y:100 * Math.sin(4 * Math.PI / N), size:20, color:colors[0]},
+        {id:'2', label:'User', x:100 * Math.cos(8 * Math.PI / N), y:100 * Math.sin(8 * Math.PI / N), size:20, color:colors[1]},
+        {id:'3', label:'User', x:100 * Math.cos(16 * Math.PI / N), y:100 * Math.sin(16 * Math.PI / N), size:20, color:colors[1]},
+        {id:'4', label:'Number', x:100 * Math.cos(32 * Math.PI / N), y:100 * Math.sin(32 * Math.PI / N), size:20, color:colors[2]},
+        {id:'5', label:'Number', x:100 * Math.cos(64 * Math.PI / N), y:100 * Math.sin(64 * Math.PI / N), size:20, color:colors[2]},
+        {id:'6', label:'APP', x:100 * Math.cos(128 * Math.PI / N), y:100 * Math.sin(128 * Math.PI / N), size:20, color:colors[3]},
+        {id:'7', label:'Website', x:100 * Math.cos(256 * Math.PI / N), y:100 * Math.sin(256 * Math.PI / N), size:20, color:colors[4]});
 
     g.edges.push(
         {id:'0', label: 'called', source: '2', target: '4', size: 2, color:'#ccc', type: 'curvedArrow'},
