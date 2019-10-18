6fd6c65e2d098ed3241ba6573c9250e6c6d305ea
LMCNN <lmcn@vt.edu>
2019-10-18 18:13:00 +0800

    adding id to the message

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     adjustRatio(canvasRatio);
 
-    //get the graph number from the server
+    //it gets the total graph number from the server
     $.ajax({
         type: 'GET',
         url: '/rendNav',
@@ -17,11 +17,11 @@ $(document).ready(function () {
         },
         error: function(xhr) {
             //Do Something to handle error
-            alert('nav bar render failed!');
+            alert('navBar rendering is failed!');
         }
     });
 
-    //add graph link to html nav bar
+    //This function adds the graph link to html navBar section
     function renderNavBar(data){
         for (let i = 0; i < data.length; i++){
             let name = data[i];
@@ -30,7 +30,7 @@ $(document).ready(function () {
         }
     }
 
-    //change the graph need to load
+    //This function changes the graph which needs to be rendered
     function switchGraph() {
         $('.navBtn').click(function(event) {
             currId =  $(this).attr('id');
@@ -43,14 +43,14 @@ $(document).ready(function () {
                 success: renderGraph,
                 error: function(xhr) {
                     //Do Something to handle error
-                    alert('Select graph failed!');
+                    alert('Connecting Error\nSelect graph failed!');
                 }
             });
 
             return false; // for good measure
         });
 
-        //search graph to render
+        //This function searches the graph need to be rendered
         $("#searchForm").submit(function (e) {
             e.preventDefault();
             let currGraph = $("input:first").val();
@@ -67,8 +67,9 @@ $(document).ready(function () {
         });
     }
 
-    //using the sigma js to render the graph which received from the server
+    //This function uses the vis.js to render the graph which is received from the server
     function renderGraph(graph) {
+        if (typeof graph.gid === 'undefined') return;
         g = graph;
         currId = graph.gid;
         describe(currId);
@@ -133,7 +134,7 @@ $(document).ready(function () {
         });
 
         network.on("selectNode", function (params) {
-            console.log(this.getNodeAt(params.pointer.DOM));
+            // console.log(this.getNodeAt(params.pointer.DOM));
             let id = this.getNodeAt(params.pointer.DOM);
             let currNode;
             for (let i = 0; i < g.nodes.length; i++) {
@@ -142,23 +143,24 @@ $(document).ready(function () {
                     break;
                 }
             }
-            let key = currNode.label + ':' + currNode.title;
-            let data = description[key];
-            if (typeof(data) !== 'undefined') {
-                console.log(data);
-                $('#msgRoot').remove();
-                $('#msg').append('<div id="msgRoot"></div>');
-
-                // Get DOM-element for inserting json-tree
-                let wrapper = document.getElementById("msgRoot");
-
-                // Create json-tree
-                let tree = jsonTree.create(data, wrapper);
-            }
+            document.getElementById(currNode.id).focus({preventScroll:true});
+            // let key = currNode.label + ':' + currNode.title;
+            // let data = description[key];
+            // if (typeof(data) !== 'undefined') {
+            //     // console.log(data);
+            //     $('#msgRoot').remove();
+            //     $('#msg').append('<div id="msgRoot"></div>');
+            //
+            //     // Get DOM-element for inserting json-tree
+            //     let wrapper = document.getElementById("msgRoot");
+            //
+            //     // Create json-tree
+            //     let tree = jsonTree.create(data, wrapper);
+            // }
         });
     }
 
-    //select graph to describe
+    //This function describes the graph which was selected by the user
     function describe(currGraph){
         $.ajax({
             type: 'GET',
@@ -166,24 +168,47 @@ $(document).ready(function () {
             dataType: 'text',
             success: showMessage,
             error: function(xhr) {
-                alert('error');
+                alert('Connecting error');
             }
         });
     }
 
-    //show the message in the message part
+    //showing the message in the message part
     function showMessage(msg) {
         let strings = msg.split('\n');
         $('#msgRoot').remove();
         $('#msg').append('<div id="msgRoot"></div>');
         // $('#msgRoot').html(msg);
+        let count = 0;
+        console.log(g);
         strings.forEach(function (line) {
-            $('#msgRoot').append(line + '<br>');
+            let strs = line.split(' ');
+            if (line[line.length - 2] === ':') {
+                let count = strs.length - 1;
+                let name = strs[count].substring(0, strs[count].length - 2);
+                count--;
+                while (strs[count] !== 'vertex') {
+                    name = strs[count] + ' ' + name;
+                    count--
+                }
+                let currNode;
+                for (let i = 0; i < g.nodes.length; i++) {
+                    if (g.nodes[i].title === name) {
+                        currNode = g.nodes[i];
+                        break;
+                    }
+                }
+                console.log(name);
+                $('#msgRoot').append('<a id=' + currNode.id + '>' + line + '</a>' + '<br>');
+            }
+            else {
+                $('#msgRoot').append('<a>' + line + '</a>' + '<br>');
+            }
         });
         getJSON();
     }
 
-    //get the json file from the server
+    //getting the json file from the server
     function getJSON() {
         $.ajax({
             type: 'GET',
@@ -191,7 +216,7 @@ $(document).ready(function () {
             dataType: 'json',
             success: function (data) {
                 description = data;
-                console.log(description);
+                // console.log(description);
             },
             error: function(xhr) {
                 alert('Get JSON failed!');
@@ -199,7 +224,7 @@ $(document).ready(function () {
         });
     }
 
-    // functions for render canvas
+    // functions for rendering the canvas
     //-------------------------------------------------
     function adjustRatio(ratio) {
         let graphStyle = {};
